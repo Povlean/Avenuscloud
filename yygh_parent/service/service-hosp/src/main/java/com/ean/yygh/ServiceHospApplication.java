37fabce248eefa88acf0c80d6ba0cbe629fd6c5b
Povlean <1927079760@qq.com>
2023-07-13 21:53:44 +0800

    7/13 second

---
+++ yygh_parent/service/service-hosp/src/main/java/com/ean/yygh/ServiceHospApplication.java
@@ -0,0 +1,15 @@
+package com.ean.yygh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:服务主启动类
+ * @author:Povlean
+ */
+@SpringBootApplication
+public class ServiceHospApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHospApplication.class,args);
+    }
+}
