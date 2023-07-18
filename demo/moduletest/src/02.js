d00d12c0f25dad3408bb4cfe3ce28d412ed220af
Povlean <1927079760@qq.com>
2023-07-18 17:09:16 +0800

    7/18

---
+++ demo/moduletest/src/02.js
@@ -0,0 +1,4 @@
+import user from './01.js'
+user.save();
+user.list();
+console.log(user.add());
---
+++ demo/webpacktest/src/01.js
@@ -0,0 +1,13 @@
+export default  {
+    save() {
+        console.log('save')
+    },
+    list() {
+        console.log('list')
+    },
+    add() {
+        var num = 1
+        num += 1
+        return num
+    }
+}
